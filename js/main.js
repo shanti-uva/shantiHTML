dc9034d086414db4e14b8bf4898c1481d5bdaa71
mark ferrara <mf8yk@virginia.edu>
2014-02-22 09:29:55 -0600

    Search panel updates
    Search panel updates and cleanup,
    - css and html class/id name changes
    - search panel now slides with top sliding collections panel
    - added js for toggling advanced text-link
    - cleaned up css
    - accordion tabs now have edge-edge click functionality
    - moved js functions to min.’s

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 
+// sliding search panel
+jQuery(function($) {
+  $("#extruder-search").buildMbExtruder({
+        positionFixed:false,
+        position:"right",
+        width:320,
+        top:0
+    });
+});
+
+// advanced search toggle
+jQuery(function($) {
+	$(".advanced-link").click(function() {
+    $(".advanced-link-view").toggle("show");
+  });
+});
