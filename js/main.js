f08fb5459532731acc874a8cd806b819e4391dd7
mark ferrara <mf8yk@virginia.edu>
2014-02-22 20:19:14 -0600

    search panel updates, icons
    search panel updates, icons

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-// sliding search panel
+// *** sliding search panel
 jQuery(function($) {
   $("#extruder-search").buildMbExtruder({
         positionFixed:false,
@@ -9,9 +9,40 @@ jQuery(function($) {
     });
 });
 
-// advanced search toggle
+
+// *** advanced search toggle
 jQuery(function($) {
 	$(".advanced-link").click(function() {
     $(".advanced-link-view").toggle("show");
   });
+});
+
+
+// *** inserts search icon for sliding panel
+jQuery(function($) {
+	$(".flap").append("<span style='font-size:1.32em;position:relative;top:-35px;left:9px;'><i class='fa fa-search'></i></span>");
+});
+
+
+
+
+
+
+jQuery(function($) {	
+			  $("#extruder-search .panel-heading").click(function() {					      			
+
+							$(this).find("i").toggleClass("fa-plus fa-minus");
+							$("#extruder-search .panel-collapse.in").prev().find("i").toggleClass("fa-plus false");							
+							$("#extruder-search .panel-collapse.in").prev().find("i").toggleClass("fa-minus true");
+							
+				});	
+
+							$("#extruder-search .panel-collapse.in").prev().find("i.fa").removeClass("fa-plus");				
+							$("#extruder-search .panel-collapse.in").prev().find("i.fa").addClass("fa-minus");
+							
+							if ($("#extruder-search .panel-collapse.in").length ){					      													
+										$(this).prev().find("i").toggleClass("fa-minus true");
+										$(this).prev().find("i").toggleClass("fa-plus false");			
+							}
+								
 });
