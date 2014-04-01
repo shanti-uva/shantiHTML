a83c1d0c7258f3d31c94299b43c4ad2cf8a061ad
mark ferrara <mf8yk@virginia.edu>
2014-04-01 13:34:46 -0600

    update, new select dropdown,
    update, new bootstrap-select

--- js/main.js
+++ js/main.js
@@ -255,69 +255,9 @@ jQuery(function($) {
 
 // *** SEARCH *** prevent flash onload
 jQuery(function ($) {
+	$(".selectpicker").selectpicker();
 	$(".input-section, .view-section, .view-section .nav-tabs>li>a").css("display","block");
 });
 		
-
-
-
-jQuery(function ($) {
-	// var select = $("select").selectBoxIt({ populate: '{ "text": "Testing shit", "value": "yea buddy", "class": "please add", "data-iconurl": "https://twimg0-a.akamaihd.net/profile_images/2633978789/80508321d8ce3ba8aa264380bb7eba33.png", "selected": "selected" }' }).data("selectBox-selectBoxIt");
-	
-  // var select = $("select").selectBoxIt({ populate: '<option value="test">test</option>' }).data('selectBox-selectBoxIt').remove(0);
-
-  $('#selector1').selectBoxIt({ 
-  
-  	showEffect: "slideDown", 
-  	showEffectSpeed: "fast",
-  	nativeMousedown: true,
-  	aggressiveChange: true,
-  	theme: "bootstrap"
-	  
-  });
-
-  // $('form').removeClass('hidden');
-
-  // $('select').last().selectBoxIt({
-  //   defaultText: "Greg Franko Repos",
-  //   populate: function() {
-  //       var deferred = $.Deferred(),
-  //           arr = [],
-  //           x = -1;
-  //       $.ajax({
-  //           url: 'https://api.github.com/users/gfranko/repos'
-  //       }).done(function(data) {
-  //           while(++x < data.length) {
-  //               arr.push(data[x].name);
-  //           }
-  //           //console.log('arr', arr);
-  //           deferred.resolve(arr);
-  //       });
-  //       return deferred;
-  //   }
-  // });
-
-  // $.ajax({
-  //   url: 'https://api.github.com/users/gfranko',
-  //   success: function(data) {
-  //       console.log('data', data);
-  //   }
-  // });
-
-  // $.ajax({
-  //   url: 'https://api.github.com/users/gfranko/repos'
-  // }).done(function(data) {
-  //   console.log('data', data);
-  // });
-
-  // $.get('https://api.github.com/users/gfranko/repos').done(function(data) {
-  //   console.log('data', data);
-  // });
-
-  //select.refresh();
-
-  //select.wait(1000, select.open).wait(1000, select.moveDown);
-
-});
 
 
