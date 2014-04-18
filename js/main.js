6e550cf191bbc7ca40f9e19d5b97816d0d9245ce
mark ferrara <mf8yk@virginia.edu>
2014-04-18 12:25:20 -0500

    added new local json for testing feature tree
    added new local json for testing feature tree

--- js/main.js
+++ js/main.js
@@ -490,7 +490,46 @@ jQuery(function ($) {
 
 
 // *** SEARCH *** clear search input & support for placeholder
-jQuery(function($) {				
+jQuery(function($) {	
+	var treeData = [
+		{title: "item1 with key and tooltip", tooltip: "Look, a tool tip!" },
+		{title: "item2: selected on init", selected: true },
+		{title: "Folder", folder: true, key: "id3",
+			children: [
+				{title: "Sub-item 3.1",
+					children: [
+						{title: "Sub-item 3.1.1", key: "id3.1.1" },
+						{title: "Sub-item 3.1.2", key: "id3.1.2" }
+					]
+				},
+				{title: "Sub-item 3.2",
+					children: [
+						{title: "Sub-item 3.2.1", key: "id3.2.1" },
+						{title: "Sub-item 3.2.2", key: "id3.2.2" }
+					]
+				}
+			]
+		},
+		{title: "Document with some children (expanded on init)", key: "id4", expand: true,
+			children: [
+				{title: "Sub-item 4.1 (active on init)", activate: true,
+					children: [
+						{title: "Sub-item 4.1.1", key: "id4.1.1" },
+						{title: "Sub-item 4.1.2", key: "id4.1.2" }
+					]
+				},
+				{title: "Sub-item 4.2 (selected on init)", selected: true,
+					children: [
+						{title: "Sub-item 4.2.1", key: "id4.2.1" },
+						{title: "Sub-item 4.2.2", key: "id4.2.2" }
+					]
+				},
+				{title: "Sub-item 4.3 (hideCheckbox)", hideCheckbox: true },
+				{title: "Sub-item 4.4 (unselectable)", unselectable: true }
+			]
+		}
+	];
+				
 	// Initialize Fancytree
 	$("#feature-tree").fancytree({
 		extensions: ["glyph", "edit", "filter"],
@@ -540,8 +579,9 @@ jQuery(function($) {
 				// loading: "icon-spinner icon-spin"
 			}
 		},
-				// source: {url: "ajax-tree-plain.json", debugDelay: 1000},
-		source: {url: "./js/fancy_nested.json", debugDelay: 1000},
+		// source: {url: "ajax-tree-plain.json", debugDelay: 1000},
+		// source: {url: "./js/fancy_nested.json", debugDelay: 1000},
+		source: treeData,
 		filter: {
 				//	mode: "hide"
 		},
